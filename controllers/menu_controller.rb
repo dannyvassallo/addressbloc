f4a15aed58d7e0d49a53d862363a37c4e74e41c1
dannyvassallo <danielvassallo87@gmail.com>
2015-07-06 12:59:56 -0400

    added nuked

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,7 +15,8 @@ class MenuController
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
     puts "5 - View Entry \"n\""
-    puts "6 - Exit"
+    puts "6 - NUKE IT ALL"
+    puts "7 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -41,6 +42,10 @@ class MenuController
       view_entry
       main_menu
     when 6
+      system "clear"
+      nuke
+      main_menu
+    when 7
       puts "Good-bye!"
       exit(0)
       system "clear"
@@ -137,6 +142,11 @@ class MenuController
   def delete_entry(entry)
     @address_book.entries.delete(entry)
     puts "#{entry.name} has been deleted"
+  end
+
+  def nuke
+    @address_book.entries.clear
+    puts "NUKED!"
   end
 
   def search_submenu(entry)
